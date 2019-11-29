cb185bc1cd84acbe6bd0eac1ef2c035d0adb0df6
David Pineda <david.pineda@deparadise.com>
2019-11-29 15:46:47 -0800

    started default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
 // SITE GENERATOR GULP TASK INDEX
+
+function defaultTask(cb) {
+	// DEFUALT when run as: gulp
+	console.log('>> GULP has started work...');
+	cb();
+}
+
+exports.default = defaultTask;
